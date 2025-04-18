c3c4413dbd4b0525cabf502c1ed150d19e720735
ltb <ltb@DESKTOP-FO8H3TR>
2025-04-19 01:00:10 +0800

    你好

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <Eigen/Core>
+#include <armadillo>
+
+using namespace std;
+using namespace Eigen;
+using namespace arma;
+
+int main() {
+    vec AX(1000);
+
+    cout<<"Ax:"<<endl;
+    cout<< AX.size()<<endl;
+
+
+    std::cout << "Hello, World!" << std::endl;
+    system("pause");
+    return 0;
+}
